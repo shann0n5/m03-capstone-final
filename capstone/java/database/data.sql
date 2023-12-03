9c884d1fc288c183abb05d95aad2c790405c5580
Keena Carulla <keenacarulla@gmail.com>
2023-12-02 22:41:04 -0500

    started working on schema script to add tables and on data script to insert test data

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -2,5 +2,20 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users ()
+
+INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES ('123 Test St, New York, NY 10024'),
+INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES (),
+INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES (),
+
+INSERT INTO applications(applicant_id, property_id, status) VALUES (),
+INSERT INTO applications(applicant_id, property_id, status) VALUES (),
+INSERT INTO applications(applicant_id, property_id, status) VALUES (),
+
+INSERT INTO service_requests (tenant_id, property_id, request_details, status) VALUES ();
+INSERT INTO service_requests (tenant_id, property_id, request_details, status) VALUES ();
+INSERT INTO service_requests (tenant_id, property_id, request_details, status) VALUES ();
+
+INSERT INTO bills(bill_id, tenant_id, property_id, amount, is_paid) VALUES (),
 
 COMMIT TRANSACTION;
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, properties, applications, service_requests, bills;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +9,38 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE properties (
+    property_id serial PRIMARY KEY,
+    address varchar(75) NOT NULL UNIQUE,
+    owner_id int REFERENCES users(user_id),
+	number_of_rooms int NOT NULL,
+	rent Decimal NOT NULL,
+	is_available boolean NOT NULL
+);
+
+CREATE TABLE applications (
+	application_id serial PRIMARY KEY,
+	applicant_id int REFERENCES users(user_id),
+	property_id int REFERENCES properties(property_id),
+	status varchar(25) NOT NULL
+);
+
+CREATE TABLE service_requests (
+	service_request_id serial PRIMARY KEY,
+	tenant_id int REFERENCES users(user_id),
+	property_id int REFERENCES properties(property_id),
+	request_details varchar(500) NOT NULL,
+	status varchar(25) NOT NULL
+);
+
+CREATE TABLE bills (
+	bill_id serial PRIMARY KEY,
+	tenant_id int REFERENCES users(user_id),
+	property_id int REFERENCES properties(property_id),
+	amount Decimal(10,2) NOT NULL,
+	is_paid boolean NOT NULL
+);
+
 
 COMMIT TRANSACTION;
