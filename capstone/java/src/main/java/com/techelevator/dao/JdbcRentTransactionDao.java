b0ab58da632511f56442a9ceec2b529dace35c86
Shannon Satterfield <satt.66qnnon@gmail.com>
2023-12-05 06:24:27 -0800

    started on the JdbcRentTransaction

--- capstone/java/src/main/java/com/techelevator/dao/JdbcRentTransactionDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/JdbcRentTransactionDao.java
@@ -1,4 +1,35 @@
 package com.techelevator.dao;
 
-public class JdbcRentTransactionDao {
+import com.techelevator.model.RentTransaction;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.List;
+
+public class JdbcRentTransactionDao implements RentTransactionDao {
+    public List<RentTransaction> getRentTransactions() {
+
+    }
+
+    public RentTransaction getRentTransactionById(int transactionId)
+
+    public List<RentTransaction> getRentTransactionsPastDue(boolean isPastDue)
+
+    public List<RentTransaction> getRentTransactionsByProperty(int propertyId)
+
+    public RentTransaction createRentTransaction(RentTransaction rentTransaction)
+
+    public RentTransaction updateRentTransaction(RentTransaction rentTransaction)
+
+    public int deleteRentTransactionById(int rentTransaction) {
+
+    }
+
+    private RentTransaction mapRowToTransfer(SqlRowSet results) {
+        RentTransaction rentTransaction = new RentTransaction(
+                results.getInt("transaction_id"),
+                results.getBigDecimal("amount"),
+                results.getDate("due_date"),
+                results.getBoolean("past_due"));
+        return rentTransaction;
+    }
 }
