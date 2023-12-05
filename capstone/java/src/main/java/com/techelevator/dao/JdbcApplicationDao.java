0026c2a49d9a3a88c63b57b46d2ed35887356fa6
Keena Carulla <keenacarulla@gmail.com>
2023-12-05 10:05:49 -0500

    added empty constructor to Applications Dao

--- capstone/java/src/main/java/com/techelevator/dao/JdbcApplicationDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/JdbcApplicationDao.java
@@ -1,10 +1,52 @@
 package com.techelevator.dao;
 
 import com.techelevator.Application;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.util.List;
 
-public class JdbcApplicationDao {
+public class JdbcApplicationDao implements ApplicationDao{
 
+    private final JdbcTemplate jdbcTemplate;
 
+    public JdbcApplicationDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Application> getApplications() {
+        return null;
+    }
+
+    @Override
+    public Application getApplicationById(int applicationId) {
+        return null;
+    }
+
+    @Override
+    public List<Application> getApplicationsByStatus(String status) {
+        return null;
+    }
+
+    @Override
+    public Application createApplication(Application application) {
+        return null;
+    }
+
+    @Override
+    public Application updateApplication(Application application) {
+        return null;
+    }
+
+    @Override
+    public int deleteApplication(int applicationId) {
+        return 0;
+    }
+
+    private Application mapToRowApplication(SqlRowSet results){
+        Application application = new Application();
+        application.setApplicationId(results.getInt());
+        return application;
+    }
 }
