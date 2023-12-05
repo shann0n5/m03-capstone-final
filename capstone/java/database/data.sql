79b718f90a932b8157ee10248b69371d45442027
Keena Carulla <keenacarulla@gmail.com>
2023-12-05 00:39:35 -0500

    edited schema script and added tables(user properties, user, profiles, tenant service requests, and tenant rent transactions, rent transactions. also started editing/adding test dat to the data script

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,28 +1,31 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN'); --userId 1001
+INSERT INTO users (username,password_hash,role) VALUES ('admin2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN'); --userId 1002
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1002
-INSERT INTO users (username,password_hash,role) VALUES ('user1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1003
-INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1004
+INSERT INTO users (username,password_hash,role) VALUES ('harry','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1003
+INSERT INTO users (username,password_hash,role) VALUES ('neville','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1004
+INSERT INTO users (username,password_hash,role) VALUES ('ron','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1005
+INSERT INTO users (username,password_hash,role) VALUES ('hermione','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1006
+INSERT INTO users (username,password_hash,role) VALUES ('ginny','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --userId 1007
 
-INSERT INTO users (username,password_hash,role) VALUES ('tenant','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_TENANT'); --userId 1005
-INSERT INTO users (username,password_hash,role) VALUES ('tenant1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_TENANT'); --userId 1006
-INSERT INTO users (username,password_hash,role) VALUES ('tenant2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_TENANT'); --userId 1007
+INSERT INTO properties(address, number_of_rooms, rent, is_available) VALUES ('123 Test St, New York, NY 10024', 3, 5000.00, true); --propertyId 2001
+INSERT INTO properties(address, number_of_rooms, rent, is_available) VALUES ('456 Testing Blvd, Long Beach, CA 90210', 2, 3500.00, false); --propertyId 2002
+INSERT INTO properties(address, number_of_rooms, rent, is_available) VALUES ('789 Test Ave, Philadelphia, PA 19093', 1, 1500.00, false); --propertyId 2003
 
-INSERT INTO users (username,password_hash,role) VALUES ('owner','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_PROPERTY_OWNER'); --userId 1008
-INSERT INTO users (username,password_hash,role) VALUES ('owner1','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_PROPERTY_OWNER'); --userId 1009
-INSERT INTO users (username,password_hash,role) VALUES ('owner2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_PROPERTY_OWNER'); --userId 1010
+INSERT INTO user_profiles(tenant_id, full_name, property_id, suite_number, has_autopay) VALUES (1003, 'Harry Potter', 2001, '3R', true);
+INSERT INTO user_profiles(tenant_id, full_name, property_id, suite_number, has_autopay) VALUES (1004, 'Neville Longbottom', 2002, NULL, true);
+INSERT INTO user_profiles(tenant_id, full_name, property_id, suite_number, has_autopay) VALUES (1005, 'Ron Weasley', 2001, '3R', false);
+INSERT INTO user_profiles(tenant_id, full_name, property_id, suite_number, has_autopay) VALUES (1006, 'Hermione Granger', 2003, NULL, true);
+INSERT INTO user_profiles(tenant_id, full_name, property_id, suite_number, has_autopay) VALUES (1007, 'Ginny Weasley', 2003, NULL, false);
 
+INSERT INTO manager_properties(propety_manager_id, property_id) VALUES (1001, 2001);
+INSERT INTO manager_properties(propety_manager_id, property_id) VALUES (1001, 2003);
+INSERT INTO manager_properties(propety_manager_id, property_id) VALUES (1002, 2002);
 
-INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES ('123 Test St, New York, NY 10024', 1008, 3, 5000.00, true); --propertyId 2001
-INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES ('456 Testing Blvd, Long Beach, CA 90210', 1009, 2, 3500.00, false); --propertyId 2002
-INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES ('789 Test Ave, Philadelphia, PA 19093', 1010, 1, 1500.00, false); --propertyId 2003
-INSERT INTO properties(address, owner_id, number_of_rooms, rent, is_available) VALUES ('101 Tester Rd, Queens, NY 10095', 1010, 4, 3950.00, false);--propertyId 2004
-
-INSERT INTO applications(applicant_id, property_id, status) VALUES (1002, 2001, 'STATUS_PENDING'); --applicationId 3001
-INSERT INTO applications(applicant_id, property_id, status) VALUES (1003, 2001, 'STATUS_WITHDRAWN'); --applicationId 3002
-INSERT INTO applications(applicant_id, property_id, status) VALUES (1004, 2001, 'STATUS_REJECTED'); --applicationId 3003
+INSERT INTO applications(applicant_id, property_id, status, has_roomates, roomate_names) VALUES (1002, 2001, 'STATUS_PENDING'); --applicationId 3001
+INSERT INTO applications(applicant_id, property_id, status, has_roomates, roomate_names) VALUES (1003, 2001, 'STATUS_WITHDRAWN'); --applicationId 3002
+INSERT INTO applications(applicant_id, property_id, status, has_roomates, roomate_names) VALUES (1004, 2001, 'STATUS_REJECTED'); --applicationId 3003
 
 INSERT INTO service_requests (tenant_id, property_id, request_details, status) VALUES (1005, 2002, 'Plumbing needed.', 'STATUS_COMPLETE'); --serviceResquestId 4001
 INSERT INTO service_requests (tenant_id, property_id, request_details, status) VALUES (1006, 2003, 'Exterminator needed.', 'STATUS_IN_PROGRESS'); --serviceResquestId 4002
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,8 +1,8 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, properties, applications, service_requests, bills;
+DROP TABLE IF EXISTS users, user_profiles, user_properties, properties, applications, service_requests, tenant_service_requests, rent_transactions, tenant_rent_transactions;
 
-DROP SEQUENCE IF EXISTS seq_user_id, seq_property_id, seq_application_id, seq_service_request_id, seq_bill_id;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_property_id, seq_application_id, seq_service_request_id, seq_transaction_id;
 
 CREATE SEQUENCE seq_user_id
 	INCREMENT BY 1
@@ -16,7 +16,7 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_users PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username),
-	CONSTRAINT CK_role CHECK (role IN ('ROLE_USER', 'ROLE_TENANT', 'ROLE_ADMIN', 'ROLE_PROPERTY_OWNER'))
+	CONSTRAINT CK_role CHECK (role IN ('ROLE_USER','ROLE_ADMIN'))
 );
 
 CREATE SEQUENCE seq_property_id
@@ -27,14 +27,27 @@ CREATE SEQUENCE seq_property_id
 CREATE TABLE properties (
     property_id int NOT NULL DEFAULT nextval('seq_property_id'),
     address varchar(75) NOT NULL UNIQUE,
-    owner_id int NOT NULL,
 	number_of_rooms int NOT NULL,
-	rent Decimal NOT NULL,
+	rent Decimal(10,2) NOT NULL,
 	is_available boolean NOT NULL,
 	CONSTRAINT PK_properties PRIMARY KEY (property_id),
-	CONSTRAINT FK_properties_users FOREIGN KEY (owner_id) REFERENCES users (user_id),
 	CONSTRAINT UQ_address UNIQUE (address) 
-	
+);
+
+CREATE TABLE user_profiles (
+	user_id int NOT NULL,
+	full_name varchar(100) NOT NULL,
+	suite_number varchar(10),
+	has_autopay boolean DEFAULT false,
+	CONSTRAINT PK_user_profiles PRIMARY KEY (user_id),
+	CONSTRAINT FK_user_profile_users FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE user_properties (
+	user_id int NOT NULL,
+	property_id int NOT NULL,
+	CONSTRAINT FK_user_properties_users FOREIGN KEY (user_id) REFERENCES users (user_id),
+	CONSTRAINT FK_user_properties_properties FOREIGN KEY (property_id) REFERENCES properties (property_id)
 );
 
 CREATE SEQUENCE seq_application_id
@@ -44,14 +57,15 @@ CREATE SEQUENCE seq_application_id
 
 CREATE TABLE applications (
 	application_id int NOT NULL DEFAULT nextval('seq_application_id'),
-	applicant_id int NOT NULL,
+	user_id int NOT NULL,
 	property_id int NOT NULL,
 	status varchar(25) NOT NULL,
-	CONSTRAINT PK_applications PRIMARY KEY (application_id),
-	CONSTRAINT FK_applications_users FOREIGN KEY (applicant_id) REFERENCES users (user_id),
+	has_roomates boolean NOT NULL,
+	roomate_names varchar(100),
+	CONSTRAINT PK_applications PRIMARY KEY (user_id),
+	CONSTRAINT FK_applications_users FOREIGN KEY (user_id) REFERENCES users (user_id),
 	CONSTRAINT FK_applications_properties FOREIGN KEY (property_id) REFERENCES properties (property_id),
 	CONSTRAINT CK_status CHECK (status IN ('STATUS_PENDING', 'STATUS_REJECTED', 'STATUS_WITHDRAWN'))
-	
 );
 
 CREATE SEQUENCE seq_service_request_id
@@ -61,30 +75,38 @@ CREATE SEQUENCE seq_service_request_id
 
 CREATE TABLE service_requests (
 	service_request_id int NOT NULL DEFAULT nextval('seq_service_request_id'),
-	tenant_id int NOT NULL,
-	property_id int NOT NULL,
 	request_details varchar(500) NOT NULL,
 	status varchar(25) NOT NULL,
 	CONSTRAINT PK_service_requests PRIMARY KEY (service_request_id),
-	CONSTRAINT FK_service_requests_users FOREIGN KEY (tenant_id) REFERENCES users (user_id),
-	CONSTRAINT FK_service_requests_properties FOREIGN KEY (property_id) REFERENCES properties (property_id),
 	CONSTRAINT CK_status CHECK (status IN ('STATUS_OPEN', 'STATUS_IN_PROGRESS', 'STATUS_COMPLETE'))
 );
 
-CREATE SEQUENCE seq_bill_id
+CREATE TABLE tenant_service_requests (
+	tenant_id int NOT NULL,
+	service_request_id int NOT NULL,
+	CONSTRAINT FK_tenant_services_requests_users FOREIGN KEY (tenant_id) REFERENCES users (user_id),
+	CONSTRAINT FK_tenant_services_requests_service_requests FOREIGN KEY (service_request_id) REFERENCES service_requests (service_request_id)
+);
+
+CREATE SEQUENCE seq_transaction_id
 	INCREMENT BY 1
 	START WITH 5001
 	NO MAXVALUE;
 
-CREATE TABLE bills (
-	bill_id int NOT NULL DEFAULT nextval('seq_bill_id'),
-	tenant_id int  NOT NULL,
-	property_id int  NOT NULL,
+CREATE TABLE rent_transactions (
+	transaction_id int NOT NULL DEFAULT nextval('seq_transaction_id'),
 	amount Decimal(10,2) NOT NULL,
-	is_paid boolean NOT NULL,
-	CONSTRAINT PK_bills PRIMARY KEY (bill_id),
-	CONSTRAINT FK_bills_users FOREIGN KEY (tenant_id) REFERENCES users (user_id),
-	CONSTRAINT FK_bills_properties FOREIGN KEY (property_id) REFERENCES properties (property_id)
+	due_date DATE NOT NULL,
+	past_due boolean NOT NULL,
+	CONSTRAINT PK_tenant_service_requests PRIMARY KEY (transaction_id)
 );
 
+CREATE TABLE tenant_rent_transactions(
+	tenant_id int NOT NULL,
+	transaction_id int NOT NULL,
+	CONSTRAINT FK_tenant_rent_transactions_users FOREIGN KEY (tenant_id) REFERENCES users (user_id),
+	CONSTRAINT FK_tenant_rent_transactions_rent_transactions FOREIGN KEY (transaction_id) REFERENCES rent_transactions (transaction_id)
+);
+
+-- ROLLBACK;
 COMMIT TRANSACTION;
