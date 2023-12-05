8d1bcb63a3f9594f84e446cd9f727fa5a08718fb
tanea quarles <tquarle@gmail.com>
2023-12-05 01:17:44 -0500

    linking_application_page_to_registration

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -8,7 +8,7 @@
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{name: 'aboutUs'}">About Us</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{name: 'properties'}">See Rentals</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{name: 'applications'}">Apply Here</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{name: 'qualifications'}">Apply Here</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{name: 'contactUs'}">Contact Us</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     
---
+++ capstone/vue/src/views/QualificationsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2> Please read qualifications of becoming a tenant at a Real-TE property before submitting an application.</h2>
+    <p>ddddddddddddeeeeeeeeeeeeettttttttttaaaaaaaaaiiiiiii
+      lllllllllllllllsssssssssssss
+      dddddddddddd
+     </p>
+     <div class="application-link-to-register">
+      <router-link v-bind:to="{name: 'register'}">Register to Apply</router-link>
+      <router-link v-bind:to="{name: 'application'}">Rental Application</router-link>
+    </div>
+
+
+  </template>
+  
+  <script>
+  
+  import MangementServcies from '../services/MangementServcies';
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
