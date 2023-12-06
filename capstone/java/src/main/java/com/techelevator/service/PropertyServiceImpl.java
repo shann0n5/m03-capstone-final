f1347ccaf92ada0dabfd0e1da9de6bf1539c7366
Shannon Satterfield <satt.66qnnon@gmail.com>
2023-12-05 21:54:03 -0800

    worked on the property service

--- capstone/java/src/main/java/com/techelevator/service/PropertyServiceImpl.java
+++ capstone/java/src/main/java/com/techelevator/service/PropertyServiceImpl.java
@@ -1,36 +1,52 @@
 package com.techelevator.service;
 
+import com.techelevator.exception.DaoException;
+import com.techelevator.exception.ServiceException;
+import com.techelevator.model.Address;
 import com.techelevator.model.Property;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.security.Principal;
 import java.util.List;
 
 @Service
 public class PropertyServiceImpl implements PropertyService{
+
+}
     @Override
-    public List<Property> viewAllProperties(Principal principal) {
+    public List<Property> searchProperties(Address address, int numberOfRooms, BigDecimal rent, boolean isAvailable) {
         return null;
     }
 
     @Override
     public Property viewPropertyById(Principal principal, int propertyId) {
-        return null;
+        try {
+            Property property =
+            return property;
+        } catch (DaoException e) {
+            throw new ServiceException("An error has occurred: " + e.getMessage());
+
+
     }
 
+//    @Override
+//    public List<Property> viewPropertiesByNumberOfRooms(Principal principal, int numberOfRooms) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Property> viewPropertiesByZipcode(Principal principal, String zip) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Property> viewPropertiesByAvailability(Principal principal, boolean isAvailable) {
+//        return null;
+//    }
     @Override
-    public List<Property> viewPropertiesByNumberOfRooms(Principal principal, int numberOfRooms) {
-        return null;
-    }
+    public List<Property> viewPropertiesByUsername(Principal principal, String username) {
 
-    @Override
-    public List<Property> viewPropertiesByZipcode(Principal principal, String zip) {
-        return null;
-    }
-
-    @Override
-    public List<Property> viewPropertiesByAvailability(Principal principal, boolean isAvailable) {
-        return null;
     }
 
     @Override
