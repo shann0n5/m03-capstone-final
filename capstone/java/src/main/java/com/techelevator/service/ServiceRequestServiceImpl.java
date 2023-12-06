91b6e231e5e744ff09392d676d51ba7f055f0889
Shannon Satterfield <satt.66qnnon@gmail.com>
2023-12-06 06:07:17 -0800

    added a return statement

--- capstone/java/src/main/java/com/techelevator/service/ServiceRequestServiceImpl.java
+++ capstone/java/src/main/java/com/techelevator/service/ServiceRequestServiceImpl.java
@@ -3,14 +3,13 @@ package com.techelevator.service;
 import com.techelevator.dao.ServiceRequestDao;
 import com.techelevator.dao.UserDao;
 import com.techelevator.model.ServiceRequest;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.util.List;
 
 @Service
-public class ServiceRequestServiceImpl implements ServiceRequestService{+
+public class ServiceRequestServiceImpl implements ServiceRequestService{
     private ServiceRequestDao serviceRequestDao;
     private UserDao userDao;
 
@@ -21,7 +20,6 @@ public class ServiceRequestServiceImpl implements ServiceRequestService{+
 
     @Override
     public List<ServiceRequest> viewAllServiceRequests(Principal principal) {
-        List<>
         return null;
     }
 
