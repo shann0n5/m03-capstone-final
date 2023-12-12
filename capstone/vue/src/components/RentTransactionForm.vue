0ef45d8e2e57fc50dc0ce810f754fe49fcb97a51
tanea quarles <tquarle@gmail.com>
2023-12-12 14:48:56 -0500

    pay rent form method to check input value matches rent amount

--- capstone/vue/src/components/RentTransactionForm.vue
+++ capstone/vue/src/components/RentTransactionForm.vue
@@ -1,16 +1,16 @@
 <template>
-    <form  v-on:click.prevent="submitPayment" class="rentTransactionForm">
+    <form class="rentTransactionForm">
       <div class="form-section">
     
 
         <div v-bind:key="rentTransaction.rentTransactionId">Paymemt Due: {{ rentTransaction.amount }}</div>
           <label for="rentPayment">Amount you would like to pay:  </label>  
-          <input type="number" id="rentPayment" name="rentPayment" >
+          <input type="number" name="paymentInput" ref="paymentInput">
       </div>
       <div v-if="rentTransaction.pastDue">Is this payment past due? {{ rentTransaction.pastDue}}</div>
       <div class="serviceRequestButton">
       
-          <button class="btn-submit" type="submit" >Complete Payment</button>&nbsp;|&nbsp;
+          <button v-bind:to="{name:'rentTransaction'}" class="btn-submit" type="submit" v-on:click="submitPayment">Complete Payment</button>&nbsp;|&nbsp;
            <button class="btn-cancel" type="button" v-on:click="cancelPayment">Cancel</button>
   
       </div>
@@ -33,55 +33,65 @@ export default {
     },
     methods: {
       submitPayment() {
-      if (!this.validateTransaction()) {
-        return;
+      if (this.validateTransaction()) {
+          this.$router.push({ name: 'rentTransaction'});
       }
-      if(this.rentTransaction.rentTransactionId === 0) {
-        RentTransactionService.getRentTransactionById(this.rentTransactionId)
-        .then(response => {
-          if (response.status === 200) {
-              this.$store.commit(
-                'SET_NOTIFICATION',
-                {
-                  message: 'Thank you for your payment!',
-                  type: 'success'
-                }
-              );
-              this.$router.push({ name: 'rentTransaction'});
-            }
-        })
-        .catch(error => {
-            this.handleErrorResponse(error, 'updating');
-          });
-      }
+    
+      //   return alert('Thank you for your payment');
+      // if(this.rentTransaction.rentTransactionId === 0) {
+        // RentTransactionService.getRentTransactionById(this.transactionId)
+        // .then(response => {
+        //   if (response.status === 200) {
+        //       this.$store.commit(
+        //         'SET_NOTIFICATION',
+        //         {
+        //           message: 'Thank you for your payment!',
+        //           type: 'success'
+        //         }
+        //       );
+        //       this.$router.push({ name: 'rentTransaction'});
+        //     }
+        // })
+        // .catch(error => {
+        //     this.handleErrorResponse(error, 'updating');
+        //   });
+      // }
     },
         cancelPayment() {
       this.$router.push({ name: 'rentTransaction'});
-    },
-    handleErrorResponse(error, verb) {
-      if (error.response) {
-        this.$store.commit('SET_NOTIFICATION',
-          "Error " + verb + " rent transaction. Response received was '" + error.response.statusText + "'.");
-      } else if (error.request) {
-        this.$store.commit('SET_NOTIFICATION', "Error " + verb + " rent transaction. Server could not be reached.");
-      } else {
-        this.$store.commit('SET_NOTIFICATION', "Error " + verb + " rent transaction. Request could not be created.");
-      }
+    // },
+    // handleErrorResponse(error, verb) {
+    //   if (error.response) {
+    //     this.$store.commit('SET_NOTIFICATION',
+    //       "Error " + verb + " rent transaction. Response received was '" + error.response.statusText + "'.");
+    //   } else if (error.request) {
+    //     this.$store.commit('SET_NOTIFICATION', "Error " + verb + " rent transaction. Server could not be reached.");
+    //   } else {
+    //     this.$store.commit('SET_NOTIFICATION', "Error " + verb + " rent transaction. Request could not be created.");
+    //   }
     },
     validateTransaction() {
-      let msg = '';
-      if(this.rentTransaction.amount === this.input)
-      if (this.rentTransaction.amount.length === 0) {
-        msg += 'Rent payment amount must be more than 0. ';
-      }
+      
+      if(this.rentTransaction.amount == this.$refs.paymentInput.value)
+      {
+        alert('Thank you for your payment! ');
+        return true;
+      } else
+        {
+          alert('Please enter valid amount')}
+      return false;
 
-      if (msg.length > 0) {
-        this.$store.commit('SET_NOTIFICATION', msg);
-        return false;
-      }
-      return true;
-    },
-    
-    }
+  }
 }
-</script>
+}
+     </script>
+ <!-- // let msg = '';\ -->
+  <!-- // msg += 
+      // if (msg.length > 0) {
+      //   this.$store.commit('SET_NOTIFICATION', msg);
+      //   return false;
+      // }
+    
+    
+   
+// } -->
--- capstone/vue/src/views/ContactUsView.vue
+++ capstone/vue/src/views/ContactUsView.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1></h1>
+  <header class="header"> Contact Us!</header>
+
+  <div class="contact-section">
+    
+    <div class="email-section"> Email Us:
+      <div>office@realTEmanagement.com</div>
+    </div>
+    
+    <div class="phn-number-section"> Call Us:
+      <div>888-888-8888</div>
+    </div>
+   
+    <div class="mailing-address-section">Our Mailing Address:
+      <div>123 Tech Rd. Philadelphia, NY 90210</div>
+    </div>
+    </div>
   </template>
   
   <script>
@@ -9,5 +24,40 @@
   </script>
   
   <style>
+  .contact-section{
+    
+  border-radius: 1rem;
+  padding: 10px;
+  border: 1px;
+ 
   
+ 
+  }
+  .email-section{
+    /* background:  #6f828e;
+    padding: 10px;
+    border-radius: .75rem;
+  border: 1px; */
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #6f828e;;
+  border-color: #6f828e;;
+  
+  text-decoration: none;
+  padding: 6px 12px;
+
+  }
+  .phn-number-section{
+    color: rgba(255, 255, 255, 0.87);
+  background-color: #6f828e;;
+  border-color: #6f828e;;
+  text-decoration: none;
+  padding: 6px 12px;
+  }
+  .mailing-address-section{
+    color: rgba(255, 255, 255, 0.87);
+  background-color: #6f828e;;
+  border-color: #6f828e;;
+  text-decoration: none;
+  padding: 6px 12px;
+  }
   </style>
