39dcdc20bb24b0301687004ffdb8bc583ac1822c
shann0n5 <satt.66qnnon@gmail.com>
2024-01-23 22:45:01 -0800

    removed api key and replaced emails with sample emails screenshot of original in the workspace folder

--- capstone/java/src/main/java/com/techelevator/service/NotificationServiceImpl.java
+++ capstone/java/src/main/java/com/techelevator/service/NotificationServiceImpl.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 
 @Service
 public class NotificationServiceImpl implements NotificationService{
-    private final String SENDGRID_API_KEY = "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";
+    private final String SENDGRID_API_KEY = "---INSERT_API_KEY_HERE---";
     public void sendEmail() {
-    Email from = new Email("keena.carulla@lc.cuny.edu");
+    Email from = new Email("userapplying@sampleemail.com");
     String subject = "Your application has been sent!";
-    Email to = new Email("keenacarulla@gmail.com");
+    Email to = new Email("propertyownerreplying@sampleemail.com");
     Content content = new Content("text/plain", "Your application has been sent!");
     Mail mail = new Mail(from, subject, to, content);
 
